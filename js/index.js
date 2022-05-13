0f4f4614c4a91b086ff8c85920d228b77fdf62fd
dario vega <sergiodariovega@gmail.com>
2022-05-13 12:10:01 -0300

    v 1.0

---
+++ js/index.js
@@ -0,0 +1,39 @@
+//captura de los botones
+let btnEncriptar = document.querySelector("#btn-encriptar");
+let btnDesencriptar = document.querySelector("#btn-desencriptar");
+let btnCopiar = document.querySelector("#btn-copiar");
+
+//agrego un evento click al botón encriptar
+btnEncriptar.addEventListener("click", () => {
+  //capturo el texto ingresado
+  let texto = capturaTexto();
+
+  //encriptar
+  let textoEncriptado = encriptaTexto(texto);
+
+  //oculto imagenes
+  ocultaImagenes();
+
+  //asigno el texto al p que quiero mostrar
+  asignaTexto(textoEncriptado);
+
+  //limpio el input text
+  limpiaTexto();
+});
+
+//agrego un evento click al botón copiar
+btnCopiar.addEventListener("click", () => {
+  copiaTextoEncriptado();
+});
+
+btnDesencriptar.addEventListener("click", () => {
+  let texto = capturaTexto();
+
+  let textoDesencriptado = desencriptaTexto(texto);
+
+  ocultaImagenes();
+
+  asignaTexto(textoDesencriptado);
+
+  limpiaTexto();
+});
---
+++ js/procesa-texto.js
@@ -0,0 +1,116 @@
+//Capturo el texto ingresado
+const capturaTexto = () => {
+  return document.querySelector("#txt-texto").value;
+};
+
+//asigno el texto al textarea que quiero mostrar
+const asignaTexto = (texto) => {
+  let pTexto = document.querySelector("#p-texto");
+  pTexto.textContent = texto;
+};
+
+//encripta texto
+const encriptaTexto = (texto) => {
+  let diccionario = {
+    a: "ai",
+    e: "enter",
+    i: "imes",
+    o: "ober",
+    u: "ufat",
+  };
+
+  let palabra = "";
+
+  for (let letra of texto) {
+    if (diccionario.hasOwnProperty(letra)) {
+      switch (letra) {
+        case "a":
+          letra = diccionario.a;
+          break;
+        case "e":
+          letra = diccionario.e;
+          break;
+        case "i":
+          letra = diccionario.i;
+          break;
+        case "o":
+          letra = diccionario.o;
+          break;
+        case "u":
+          letra = diccionario.u;
+          break;
+      }
+    }
+    palabra += letra;
+  }
+
+  return palabra;
+};
+
+//limpia el texto ingresado por el usuario
+const limpiaTexto = () => {
+  return (document.querySelector("#txt-texto").value = "");
+};
+
+//ocultar imágenes
+const ocultaImagenes = () => {
+  let imgDesaparece = document.querySelector("#img-desaparece");
+  let pTexto = document.querySelector("#p-texto");
+
+  imgDesaparece.style.display = "none";
+  pTexto.style.display = "block";
+  btnCopiar.style.visibility = "visible";
+  btnCopiar.classList.add("aparece");
+};
+
+//copiar en portapapeles texto encriptado
+const copiaTextoEncriptado = () => {
+  let pTexto = document.querySelector("#p-texto");
+  pTexto.select();
+  document.execCommand("copy");
+};
+
+//limpia texto encriptado
+const limpiaTextoEncriptado = () => {
+  return (document.querySelector("#p-texto").value = "");
+};
+
+//desencripta texto
+const desencriptaTexto = (texto) => {
+  limpiaTextoEncriptado();
+
+  let diccionario = {
+    ai: "a",
+    enter: "e",
+    imes: "i",
+    ober: "o",
+    ufat: "u",
+  };
+
+  let palabra = "";
+
+  for (let letra of texto) {
+    if (diccionario.hasOwnProperty(letra)) {
+      switch (letra) {
+        case "ai":
+          letra = diccionario.ai;
+          break;
+        case "enter":
+          letra = diccionario.enter;
+          break;
+        case "imes":
+          letra = diccionario.imes;
+          break;
+        case "ober":
+          letra = diccionario.ober;
+          break;
+        case "ufat":
+          letra = diccionario.ufat;
+          break;
+      }
+    }
+    palabra += letra;
+  }
+
+  return palabra;
+};
